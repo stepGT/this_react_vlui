0bd4f4367b18481b9a29fd45a0445c7b5a3cc026
stepGT <stepGT@mail.ru>
2022-01-20 02:40:23 +1000

    Added Content Template

---
+++ src/components/ContentHeader.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { Form, Row, Container } from 'react-bootstrap';
+
+const ContentHeader = ({ data, description, head }) => {
+  const keys = data ? Object.keys(data) : [];
+  const [valueRadio, setValueRadio] = useState('version');
+  const handleChange = (e) => {
+    setValueRadio(e.target.value);
+  };
+  return (
+    <Container>
+      <div className="starter-template text-center mt-5">
+        <h1>{head}</h1>
+        <p className="lead text-capitalize">{description}</p>
+        <Row className="mb-3">
+          <Form>
+            {keys &&
+              keys.map((key, index) => {
+                let checked = valueRadio === key ? true : false;
+                return (
+                  <Form.Check
+                    checked={checked}
+                    value={key}
+                    onChange={handleChange}
+                    key={index}
+                    inline
+                    type="radio"
+                    label={key}
+                  />
+                );
+              })}
+          </Form>
+        </Row>
+      </div>
+    </Container>
+  );
+};
+
+export default ContentHeader;
--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -1,14 +1,38 @@
-import Header from '../components/Header';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import ContentHeader from '../components/ContentHeader';
 import Meta from '../components/Meta';
 
+const { REACT_APP_VAL_API_KEY } = process.env;
+
+const valContentV1 = `https://eu.api.riotgames.com/val/content/v1/contents?api_key=${REACT_APP_VAL_API_KEY}`;
+
 const Content = () => {
+  const [getContent, setGetContent] = useState([]);
+  const [isFetching, setIsFetching] = useState(true);
+
+  const fetchContent = region => {
+    setIsFetching(true);
+    axios.get(`https://${region}.api.riotgames.com/val/content/v1/contents?api_key=${REACT_APP_VAL_API_KEY}`).then((res) => {
+      setGetContent(res.data);
+      setIsFetching(false);
+    });
+  }
+
+  useEffect(() => {
+    axios.get(valContentV1).then((res) => {
+      setGetContent(res.data);
+      setIsFetching(false);
+    });
+  }, []);
+
   const pageTitle = 'VAL-CONTENT-V1';
   const pageDescription = 'Get content optionally filtered by locale';
 
   return (
     <div>
       <Meta title={pageTitle} />
-      <Header head={pageTitle} description={pageDescription} />
+      <ContentHeader data={getContent} description={pageDescription} head={pageTitle} />
     </div>
   );
 };
