cdbc503c1c56569eb03582a59b6a45130d57d8b6
stepGT <stepGT@mail.ru>
2022-02-01 23:14:11 +1000

    Small fix

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux'
-import axios from 'axios';
 import { Container } from 'react-bootstrap';
 import { Routes, Route } from 'react-router-dom';
-import "./App.css";
-
 import Layout from './layout/Layout';
-
 import Content from './pages/Content';
 import Status from "./pages/Status";
-import { setValues } from './features/status/statusSlice';
-import { REGIONS } from './constants';
 
-const { REACT_APP_VAL_API_KEY } = process.env;
+import "./App.css";
 
 const App = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    const fetchData = async (region = 'br') => {
-      try {
-        const response = await axios
-          .get(
-            `https://${region}.api.riotgames.com/val/status/v1/platform-data?api_key=${REACT_APP_VAL_API_KEY}`
-          )
-          .then((res) => res.data);
-        dispatch(setValues(response));
-      } catch (error) {
-        console.log('error', error);
-      }
-    };
-    // Fetch all status data
-    setTimeout(() => REGIONS.map(el => fetchData(el)), 1000);
-  }, [dispatch]);
+  
   return (
     <Layout>
       <Container>
